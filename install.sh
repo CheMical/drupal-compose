4a276f8204a3634051262b9b4844e357c868a40e
Michael Carlsson <CheMical@users.noreply.github.com>
2019-01-30 21:15:22 +0100

    Installation of Drupal

---
+++ install.sh
@@ -0,0 +1,8 @@
+## Installation Drupal
+docker-compose run drush drush -y si --db-url=mysql://root:example@mariadb:3306/test
+
+## Enable simpletest module 
+docker-compose run drush drush -y en simpletest
+
+## Enable writing for sites/default/files due to use of Apache webserver
+docker-compose exec drupal bash -c "chmod a+w /var/www/html/sites/default/files"
